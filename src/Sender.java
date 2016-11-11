269cc43c20927e09e1b64f7834425124c72eea4e
dongwoo1005 <Ehddnths93>
2016-11-10 20:55:18 -0500

    Fix timer bug in Sender

--- src/Sender.java
+++ src/Sender.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.OpenOption;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,9 +17,9 @@ import java.util.TimerTask;
 public class Sender {
 
     static final int N = 10;
-    static final int MAXLEN = 500;
+    static final int MAXLEN = 50;
     static final int PACKET_SIZE = 512;
-    static final int TIMEOUT_DELAY = 200;
+    static final int TIMEOUT_DELAY = 50;
     static final String SEQ_LOG_FILE_NAME = "seqnum.log";
     static final String ACK_LOG_FILE_NAME = "ack.log";
 
@@ -28,12 +29,182 @@ public class Sender {
     static ArrayList<Packet> packets;
 
     static int nextSeqNum = 1;
+    static int seqNumCount;
     static int base = 1;
     static Timer timer = null;
 
     static BufferedReader bufferedReader = null;
     static BufferedWriter seqLogWriter = null;
     static BufferedWriter ackLogWriter = null;
+
+
+    private static void checkArguments(String[] args) {
+        // check number of arguments
+        if (args.length != 4) {
+            System.err.println(
+                    "Required command line parameters:"
+                            + " <host address of the nEmulator>"
+                            + " <UDP port number used by the emulator to receive data from the sender>"
+                            + " <UDP port number used by the sender to receive ACKs from the emulator>"
+                            + " <name of the file to be transferred>"
+            );
+            System.exit(1);
+        }
+    }
+
+
+    private static void initialize(String[] args) throws IOException {
+
+        // read from commandline arguments
+        nEmulatorHostAddr = args[0];
+        nEmulatorPortGetData = Integer.valueOf(args[1]);
+        senderPortGetAck = Integer.valueOf(args[2]);
+        fileName = args[3];
+
+        packets = new ArrayList<>();
+
+        bufferedReader = Files.newBufferedReader(Paths.get(fileName));
+
+        // seq log writer
+        File seqLogFile = new File(SEQ_LOG_FILE_NAME);
+        seqLogFile.delete();
+        seqLogFile.createNewFile();
+        seqLogWriter = Files.newBufferedWriter(Paths.get(SEQ_LOG_FILE_NAME), StandardOpenOption.WRITE);
+
+        // ack log writer
+        File ackLogFile = new File(ACK_LOG_FILE_NAME);
+        ackLogFile.delete();
+        ackLogFile.createNewFile();
+        ackLogWriter = Files.newBufferedWriter(Paths.get(ACK_LOG_FILE_NAME), StandardOpenOption.WRITE);
+    }
+
+
+    private static void processData() throws Exception {
+
+        seqNumCount = 0;
+        for ( ;; ) {
+
+            char[] charArr = new char[MAXLEN];
+            int result = bufferedReader.read(charArr, 0, MAXLEN);
+            if (result < 0) break;
+
+            String data = String.valueOf(charArr);
+            int emptyIndex = data.indexOf("\u0000");
+            if (emptyIndex > 0) data = data.substring(0, emptyIndex);
+
+            Packet packet = Packet.createPacket(seqNumCount + 1, data);
+            packets.add(packet);
+            seqNumCount += 1;
+        }
+    }
+
+
+    private static void reliableDataTransferSend(Packet packet) throws Exception {
+        System.out.println("rdt_send");
+        if (nextSeqNum < base + N) {
+            sendData(packet);
+        } else {
+            refuseData(packet);   // hold and wait until base++
+        }
+        System.out.println("finish rdt_send");
+        return;
+    }
+
+
+    private static void sendData(Packet packet) throws Exception {
+        System.out.println("sendData");
+        unreliableDataTransferSend(packet, nEmulatorHostAddr, nEmulatorPortGetData);
+        if (base == nextSeqNum) {
+            System.out.print("base == nextSeqNum");
+            startTimer();
+        }
+        nextSeqNum += 1;
+    }
+
+
+    private static void refuseData(Packet packet) throws Exception {
+        System.out.println("refuseData");
+        while(nextSeqNum >= base + N) {
+//            System.out.println("wait");
+        }
+        sendData(packet);
+    }
+
+
+    private static void unreliableDataTransferSend(Packet packet, String nEmulatorHostAddr, int nEmulatorPortGetData) {
+
+        System.out.println(packet.getSeqNum() + "udt_send");
+        // Create a UDP socket
+        DatagramSocket udpSocket = null;
+        try {
+            udpSocket = new DatagramSocket();
+
+            // Send packet with message
+            byte[] sendData = packet.getUDPdata();
+            InetAddress ipAddress = InetAddress.getByName(nEmulatorHostAddr);
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, nEmulatorPortGetData);
+            udpSocket.send(sendPacket);
+
+            seqLogWriter.write(packet.getSeqNum());
+            System.out.println(packet.getSeqNum() + "write seq log");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (udpSocket != null) udpSocket.close();
+            System.out.println(packet.getSeqNum() + "finish udt_send");
+        }
+    }
+
+
+    private static void startTimer() {
+        System.out.println("startTimer");
+        stopTimer();
+        timer = new Timer();
+        timer.schedule(new TimeoutTask(), TIMEOUT_DELAY);
+    }
+
+
+    private static void stopTimer() {
+        System.out.println("in stopTimer");
+        if (timer != null) {
+            System.out.println("stopped");
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
+    }
+
+
+    private static class TimeoutTask extends TimerTask {
+
+        Thread timeoutTask;
+
+        public TimeoutTask() {
+            this.timeoutTask = new Thread(this);
+        }
+
+        @Override
+        public void run() {
+            System.out.println("timertask");
+            startTimer();
+            for (int i=base; i<nextSeqNum; i+=1) {
+                unreliableDataTransferSend(packets.get(i-1), nEmulatorHostAddr, nEmulatorPortGetData);
+            }
+        }
+    };
+
+
+    private static void closeAll() throws IOException {
+        System.out.println("closeAll");
+        if (ackLogWriter != null) ackLogWriter.close();
+        if (seqLogWriter != null) seqLogWriter.close();
+        if (bufferedReader != null) bufferedReader.close();
+        stopTimer();
+    }
 
 
     static class ReceiveAckTask implements Runnable {
@@ -49,9 +220,10 @@ public class Sender {
 
             DatagramSocket getAckUdpSocket = null;
             try {
-                // Create a UDP socket on sender port to get Acks
-                getAckUdpSocket = new DatagramSocket(senderPortGetAck);
                 while (true) {
+
+                    // Create a UDP socket on sender port to get Acks
+                    getAckUdpSocket = new DatagramSocket(senderPortGetAck);
 
                     // ReliableDataTransfer Receive Packet - extract
                     byte[] receiveData = new byte[PACKET_SIZE];
@@ -65,14 +237,21 @@ public class Sender {
 
                     base = myPacket.getSeqNum() + 1;
                     if (base == nextSeqNum) stopTimer();
-                    else startTimer();
+                    else {
+                        System.out.println("receive run else");
+                        startTimer();
+                    }
+                    getAckUdpSocket.close();
                 }
 
             } catch (SocketException e) {
+                System.out.println("Receive Task Socket Exception");
                 e.printStackTrace();
             } catch (IOException e) {
+                System.out.println("Receive Task IO Exception");
                 e.printStackTrace();
             } catch (Exception e) {
+                System.out.println("Receive Task Exception Exception");
                 e.printStackTrace();
             } finally {
                 if (getAckUdpSocket != null) getAckUdpSocket.close();
@@ -90,157 +269,23 @@ public class Sender {
     }
 
 
-    static class SendDataTask {
-
-        public void run() {
-            try {
-                for ( ;; ) {
-
-                    char[] charArr = new char[MAXLEN];
-                    int result = bufferedReader.read(charArr, 0, MAXLEN);
-                    if (result < 0) break;
-
-                    String data = String.valueOf(charArr);
-                    int emptyIndex = data.indexOf("\u0000");
-                    if (emptyIndex > 0) data = data.substring(0, emptyIndex);
-
-                    reliableDataTransferSend(data);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    private static void reliableDataTransferSend(String data) throws Exception {
-        if (nextSeqNum < base + N) {
-            sendData(data);
-        } else {
-            refuseData(data);   // hold and wait until base++
-        }
-    }
-
-
-    private static void sendData(String data) throws Exception {
-        Packet packet = Packet.createPacket(nextSeqNum, data);
-        packets.add(packet);
-        unreliableDataTransferSend(packet, nEmulatorHostAddr, nEmulatorPortGetData);
-        if (base == nextSeqNum) startTimer();
-        nextSeqNum += 1;
-    }
-
-
-    private static void refuseData(String data) throws Exception {
-        while(nextSeqNum >= base + N) {}
-        sendData(data);
-    }
-
-
-    private static void unreliableDataTransferSend(Packet packet, String nEmulatorHostAddr, int nEmulatorPortGetData)
-            throws IOException {
-
-        // Create a UDP socket
-        DatagramSocket udpSocket = new DatagramSocket();
-
-        // Send packet with message
-        byte[] sendData = packet.getUDPdata();
-        InetAddress ipAddress = InetAddress.getByName(nEmulatorHostAddr);
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, nEmulatorPortGetData);
-        udpSocket.send(sendPacket);
-
-        seqLogWriter.write(packet.getSeqNum());
-
-        udpSocket.close();
-    }
-
-
-    private static void startTimer() {
-        stopTimer();
-        timer = new Timer();
-        timer.schedule(timeout, TIMEOUT_DELAY);
-    }
-
-
-    private static void stopTimer() {
-        if (timer != null) {
-            timer.cancel();
-            timer.purge();
-            timer = null;
-        }
-    }
-
-
-    private static TimerTask timeout = new TimerTask() {
-        @Override
-        public void run() {
-            startTimer();
-            for (int i=0; i<nextSeqNum; i+=1) {
-                try {
-                    unreliableDataTransferSend(packets.get(base), nEmulatorHostAddr, nEmulatorPortGetData);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    };
-
-
-    private static void closeAll() throws IOException {
-        if (ackLogWriter != null) ackLogWriter.close();
-        if (seqLogWriter != null) seqLogWriter.close();
-        if (bufferedReader != null) bufferedReader.close();
-        stopTimer();
-    }
-
-
     public static void main(String[] args) throws Exception {
 
         // Read command line arguments
-        if (args.length != 4) {
-            System.err.println(
-                    "Required command line parameters:"
-                            + " <host address of the nEmulator>"
-                            + " <UDP port number used by the emulator to receive data from the sender>"
-                            + " <UDP port number used by the sender to receive ACKs from the emulator>"
-                            + " <name of the file to be transferred>"
-            );
-            System.exit(1);
-        }
-
-        nEmulatorHostAddr = args[0];
-        nEmulatorPortGetData = Integer.valueOf(args[1]);
-        senderPortGetAck = Integer.valueOf(args[2]);
-        fileName = args[3];
-
-        packets = new ArrayList<>();
-
-        bufferedReader = Files.newBufferedReader(Paths.get(fileName));
-
-        // seq log writer
-        File seqLogFile = new File(SEQ_LOG_FILE_NAME);
-        if (seqLogFile.exists()) {
-            seqLogFile.delete();
-            seqLogFile.createNewFile();
-        }
-        seqLogWriter = Files.newBufferedWriter(Paths.get(SEQ_LOG_FILE_NAME), (OpenOption) null);
-
-        // ack log writer
-        File ackLogFile = new File(ACK_LOG_FILE_NAME);
-        if (ackLogFile.exists()) {
-            ackLogFile.delete();
-            ackLogFile.createNewFile();
-        }
-        ackLogWriter = Files.newBufferedWriter(Paths.get(ACK_LOG_FILE_NAME), (OpenOption) null);
+        checkArguments(args);
+        initialize(args);
+        processData();
 
         // Begin receiving ACKs task on a separate thread
         ReceiveAckTask receiver = new ReceiveAckTask();
         receiver.start();
 
         // Begin sending data on the main thread
-        SendDataTask sender = new SendDataTask();
-        sender.run();
+        for (int i=0; i<seqNumCount; i+=1) {
+            System.out.println("i=" + i);
+            reliableDataTransferSend(packets.get(i));
+            System.out.println("i=" + i);
+        }
+        System.out.println("done");
     }
 }
